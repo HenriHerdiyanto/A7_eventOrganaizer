de51ffc23caaf30713c488c4f9c23c132566ce4c
irfangeliga <muhammad.2960@students.amikom.ac.id>
2023-03-20 22:18:59 +0700

    henry update

--- admin/index.php
+++ admin/index.php
@@ -44,7 +44,7 @@
                       <i class="bi bi-cart"></i>
                     </div>
                     <div class="ps-3">
-                      <h6>145</h6>
+                      <h6>150</h6>
                       <span class="text-success small pt-1 fw-bold">12%</span> <span class="text-muted small pt-2 ps-1">increase</span>
 
                     </div>
