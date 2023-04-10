baf536ae128d581dedd43cdd37bac0910eb896a6
pangestu40 <pangestufremmuzar@gmail.com>
2023-04-10 13:49:03 +0700

    [Pangs] Edit Mobil

--- mobil.php
+++ mobil.php
@@ -68,63 +68,12 @@
             </ol>
           </p>
         </div>
-        <button class="btn btn-primary" type="submit">Pesan</button>
+        <button class="btn btn-success" type="submit">Chat Sekarang</button>
       </form>
     </div>
   </div>
   </section>
 
-  <section class="container border">
-  <div class="row">
-    <div class="col-4">
-      <img src="assets/img/mobil.png" alt="mobil" width="300px">
-    </div>    
-    <div class="col-8">
-      <ul class="nav nav-tabs">
-        <li class="nav-item">
-          <a class="nav-link active" data-bs-toggle="tab" href="#paket1">Paket 1</a>
-        </li>
-        <li class="nav-item">
-          <a class="nav-link" data-bs-toggle="tab" href="#paket2">Paket 2</a>
-        </li>
-        <li class="nav-item">
-          <a class="nav-link" data-bs-toggle="tab" href="#paket3">Paket 3</a>
-        </li>
-      </ul>
-      <form class="card-body tab-content">
-        <div class="tab-pane active" id="paket1">
-          <p>
-          <ol class="list-group list-group-numbered">
-              <li class="list-group-item">Tanpa Supir</li>
-              <li class="list-group-item">Bensin Sendiri</li>
-              <li class="list-group-item">Mobil Ambil Sendiri</li>
-            </ol>
-          </p>
-
-        </div>
-        <div class="tab-pane" id="paket2">
-        <ol class="list-group list-group-numbered">
-          <p>
-              <li class="list-group-item">Dengan Supir</li>
-              <li class="list-group-item">Bensin Sendiri</li>
-              <li class="list-group-item">Mobil Ambil Sendiri</li>
-            </ol>
-          </p>
-        </div>
-        <div class="tab-pane" id="paket3">
-        <ol class="list-group list-group-numbered">
-          <p>
-              <li class="list-group-item">Dengan Supir</li>
-              <li class="list-group-item">Bensin Disediakan</li>
-              <li class="list-group-item">Di Jemput</li>
-            </ol>
-          </p>
-        </div>
-        <button class="btn btn-primary" type="submit">Pesan</button>
-      </form>
-    </div>
-  </div>
-  </section>
   <br>
 
 </main><!-- End #main -->
